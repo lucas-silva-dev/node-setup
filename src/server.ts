de9a1ca076d7d4651786a36c8ec71486cc680e38
lucas-silva-dev <lucassilva.work@outlook.com>
2020-06-25 01:15:00 -0300

    Add ESlint & Jest

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,9 @@
-import express from "express";
+import express from 'express';
 
 const app = express();
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  return res.json({ message: "Hello World" });
-});
+app.get('/', (req, res) => res.json({ message: 'Hello World' }));
 
 app.listen(3333);
